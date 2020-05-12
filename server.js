0c25e6a1c2b7b46efc96b0a963be8afd9fa75d3b
MasonChance <mason.fryberger@gmail.com>
2020-05-11 17:10:13 -0700

    added all base files and initial server setup data

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const cors = requre('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors());
