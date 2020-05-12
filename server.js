24db659b7db82ebcbf8ea24a485a52061171923f
MasonChance <mason.fryberger@gmail.com>
2020-05-11 20:28:58 -0700

    server test

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const express = require('express');
-const cors = require('cors');
+const app = express();
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
 
-const app = express();
 app.use(cors());
+
+app.listen(PORT, () => console.log('I am ALIVE!!'))
