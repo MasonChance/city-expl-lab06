ee4c6d4318ffe93abd13a0ccabac411979d9f2f7
MasonChance <mason.fryberger@gmail.com>
2020-05-13 17:45:31 -0700

    api refactoring in progress

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ backend api/server excercise
 
 **Author**: Mason Fryberger [GitHub](https://github.com/MasonChance)
 
+**Version**: 1.2.0
+
 **Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
 
 ## Overview
@@ -17,39 +19,52 @@ This application will be a stand-alone app that interacts with a static front en
 
 1. Set Up file Structure including .json files and server.js
 1. run ` npm install -save ` in terminal
-1. 
+1. run ` npm install dotenv superagent express cors ` in terminal
+1.
 
 ## Architecture
 <!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
 
 ## Change Log
-Feature #2 Location Route
+### Feature 2 Location Route results w/json-file
 
-Estimate of time needed to complete: _____
+Estimate of time needed to complete: 60min
 
-Start time: _____
+Start time: Mon 5-11-2020
 
-Finish time: _____
+Finish time: Wed 5-14-202
 
-Actual time needed to complete: _____
+Actual time needed to complete: 3days
 
-01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource.
+05-13-2020 16:02 - Application now has a fully-functional express server, with a GET route for the location resource. Future patch will replace "./data/geo.json" with an API from LocationIQ.
 
-### Feature 3 /weather results w/jsonfile
+### Feature 3 /weather results w/json-file
 
 Estimate of time needed to complete: 60min
 
 Start time: 17:00
 
+Finish time: 21:00
+
+Actual time needed to complete: 4hrs
+
+05-12-2020 21:00 - Application now has response for Weather, with a GET route for the weather resource. Future patch will replace "./data/darksky.json" with API from weatherbit.
+
+### Feature 3 Implement location API
+
+Estimate of time needed to complete: 120min
+
+Start time: 16:15
+
 Finish time: _____
 
 Actual time needed to complete: _____
 
-05-12-2020 ??:?? - Application now has __________, with a GET route for the location resource.
+
+01-01-2001 4:59pm - Application now has acess to LocationIQ API, using a GET route for the location resource, utilizing 'superagent' to formulate the API query.
 
 <!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an examples:
 
-01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource.
 
 Number and name of feature: ________________________________
 
@@ -61,6 +76,7 @@ Finish time: _____
 
 Actual time needed to complete: _____
 
+01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource.
 
 
 
--- server.js
+++ server.js
@@ -7,34 +7,57 @@ const superagent = require('superagent');  // sets superagent dependency- necess
 
 const PORT = process.env.PORT;
 const cors = require('cors');  // sets cors dependency allowing server to interact with other servers and urls. 
-
-app.use(cors()); // invokes use method on express, passing in the cors invokation. 
+const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
+app.use(cors()); // invokes use method on express, passing in the cors invocation. 
 
 
 
 //===== location data with the Json ===//
 app.get('/location', (req, res) => {
-  
-  const location = require('./data/geo.json');
-  
-  res.send('Im the Information');
-  
+ /* !!! the following  lines were specific to using a json file that exists in the file directory of the App. // req = require('./data/geo.json'); // starting over with json baseline
+  // const result = new LocationData (req[0]);
+  // res.send(result);  */
+  console.log('req:', req.body);
+  //TODO: refactor to use superagent and APIquery
+  const locatIq_Url = 'https://us1.locationiq.com/v1/search.php'; //childOf locationIQ
+  const city_toBeSearched = req.query.city; // childOf front-end
+  console.log('city_toBeSearched:', city_toBeSearched);
+  const superQueryParam = {
+    q: city_toBeSearched, //childOf front-end
+    key: GEOCODE_API_KEY, 
+    format: 'json'
+  };
+  console.log('superQueryParam:', superQueryParam)
+  superagent.get(locatIq_Url)
+    .query(superQueryParam)
+    .then(result_locatIq => {
+      const newLocation = new LocationData(result_locatIq.body);
+      console.log('newLocation:',  newLocation);
+      res.send(newLocation).status(200); // `.status(200)` is arbitrary success msg
+    })
+    //TODO: needs a .catch to handle promise. 
+    .catch(error => {
+      console.log('error:', error);
+      res.send(error).status(500); // `.status(500)` is arbitrary. specific errors can be choosen but this is the default. 
+    });
+
 });
 
-/*=== access server, callback should return data mathing the following:
-[
-  {
-    "forecast": "Partly cloudy until afternoon.",
-    "time": "Mon Jan 01 2001"
-  },
-  {
-    "forecast": "Mostly cloudy in the morning.",
-    "time": "Tue Jan 02 2001"
-  },
-]
 
-*/
 
+
+
+// ==== Location data Constructor =====// 
+function LocationData(result_locatIq){
+  
+  this.search_query = city_toBeSearched;
+  this.formatted_query = result_locatIq.body.display_name;
+  this.latitude = result_locatIq.body.lat;
+  this.longitude = result_locatIq.body.lon;
+}
+
+
+// === weather data using .map and Json file ==//
 app.get('/weather', (req, res) => {
   req = require('./data/darksky.json');   
     const result = req.data.map((current) => {
@@ -44,7 +67,7 @@ app.get('/weather', (req, res) => {
 });
 
 //==== constructor for /weather data from JSON ====//
-const WeatherData = function(result){
+function WeatherData(result){
   this.forecast = result.weather.description;
   this.time = result.datetime;  
 }
@@ -54,5 +77,5 @@ const WeatherData = function(result){
 
 
 
-// ===== spins up server on local and tests it===// see https://limitless-peak-33770.herokuapp.com/ for deployed site. 
-app.listen(PORT, () => console.log('I am ALIVE!!')); // local host server test
+// ===== spins up server on local and tests it===// see https://limitless-peak-33770.herokuapp.com/ for deployed app. 
+app.listen(PORT, () => console.log(`I am ALIVE on: ${PORT} !!`)); // local host server test
