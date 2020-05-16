613aa057a4e90cbffbe874f9f3b01c0cbd1aee27
MasonChance <mason.fryberger@gmail.com>
2020-05-16 13:06:44 -0700

    sever deployment

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const app = express();
 app.use(cors()); 
 
 //==== Handlers =====///
-const homePage = require('https://codefellows.github.io/code-301-guide/curriculum/city-explorer-app/front-end/')
+
 const getLocation = require('./modules-js/location.js');
 const getForecast = require('./modules-js/weather.js');
 
 //===== Routes ===//
-app.get('/', homePage);
+
 app.get('/location', getLocation);
 app.get('/weather', getForecast);
 
